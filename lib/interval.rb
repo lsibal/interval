a96bc1a776d39a3cf6799124902e24fbec75c156
Lew Sibal <lew@egissys.com>
2017-03-15 07:39:13 +0800

    Initial work on problem 2

--- lib/interval.rb
+++ lib/interval.rb
@@ -1,11 +1,12 @@
 class Interval
   attr_accessor :start, :end
 
-  def initialize(istart = 0, iend = 0)
-    fail "Start and End times should be numeric" unless istart.is_a?(Fixnum) && iend.is_a?(Fixnum)
-    fail "Invalid interval" if istart > iend
+  def initialize(istart = 0, iend = 0, special_type = :none)
+    # fail "Start and End times should be numeric" unless istart.is_a?(Fixnum) && iend.is_a?(Fixnum)
+    # fail "Invalid interval" if istart > iend
     @start = istart
     @end = iend
+    @type = special_type
   end
 
   def contains?(interval)
@@ -19,8 +20,8 @@ class Interval
 
   def -(i)
     return nil if self == i
-    i.start = self.start if i.start < self.start
-    i.end = self.end if i.end > self.end
+    i.start = [i.start, self.start].max
+    i.end = [self.end, i.end].min
     return Interval.new(i.end + 1, self.end) if (self.start == i.start)
     return Interval.new(self.start, i.start - 1) if (self.end == i.end)
     return [Interval.new(self.start, i.start - 1), Interval.new(i.end + 1, self.end)]
@@ -30,8 +31,32 @@ class Interval
     (self.start == i.start) && (self.end == i.end)
   end
 
+  def self.time_string_compare(op, left, right)
+    lhour, lmin = left.split(':').map(&:to_i)
+    rhour, rmin = right.split(':').map(&:to_i)
+
+    lhour == rhour ? lmin.send(op, rmin) : lhour.send(op, rhour)
+  end
+
   def self.p1(a, b, x)
-    res = [].tap { |out| ((b.reject { |i| b.count(i) < x }).compact.uniq { |i| i.to_s }).each { |i| out << a - i } }.flatten
+    res = [].tap { |out| ((b.reject { |i| b.count(i) < x }).compact.uniq(&:to_s)).each { |i| out << a - i } }.flatten
     res.one? ? res.first : res
+  end
+
+  def self.p2(intervals)
+    [].tap do |out|
+      intervals.sort { |a, b| a.start <=> b.start }.each_with_index do |int, idx|
+        p2_prepare([], intervals[idx..-1], out)
+      end
+    end.uniq.reject { |x| x.empty? }
+  end
+
+  def self.p2_prepare(tmp, intervals, out)
+    intervals.each_with_index do |int, idx|
+      if tmp.empty? || time_string_compare(:<=, tmp.last.end, int.start)
+        p2_prepare(tmp + [int], intervals[idx+1..-1], out)
+      end
+    end
+    out.concat([tmp]) unless out.any? { |o| tmp.all? { |t| o.include? t } }
   end
 end
--- spec/lib/interval_spec.rb
+++ spec/lib/interval_spec.rb
@@ -5,7 +5,7 @@ require 'interval'
 
 describe Interval do
   describe "Problem #1" do
-    subject { Interval.p1(a, b, x).to_s }
+    subject { Interval.p1(a, b, x) }
 
     context 'Test #1' do
       let(:a) { Interval.new(1,100) }
@@ -16,7 +16,7 @@ describe Interval do
                  Interval.new(80,90)] }
       let(:x) { 3 }
 
-      it { should eq "51-100" }
+      it { should eq Interval.new(51, 100) }
     end
 
     context 'Test #2' do
@@ -28,7 +28,7 @@ describe Interval do
                  Interval.new(80,90)] }
       let(:x) { 2 }
 
-      it { should eq "[1-50, 1-79]" }
+      it { should eq [Interval.new(1,50), Interval.new(1,79)] }
     end
 
     context 'Test #3' do
@@ -38,7 +38,7 @@ describe Interval do
                  Interval.new(12,34)] }
       let(:x) { 3 }
 
-      it { should eq "[1-11, 35-100]" }
+      it { should eq [Interval.new(1,11), Interval.new(35,100)] }
     end
 
     context 'Test #4' do
@@ -46,7 +46,7 @@ describe Interval do
       let(:b) { [Interval.new(4,35)] }
       let(:x) { 1 }
 
-      it { should eq "36-60" }
+      it { should eq Interval.new(36,60) }
     end
 
     context 'Test #5' do
@@ -54,11 +54,28 @@ describe Interval do
       let(:b) { [Interval.new(27,72)] }
       let(:x) { 1 }
 
-      it { should eq "5-26" }
+      it { should eq Interval.new(5,26) }
     end
   end
 
   describe "Problem #2" do
+    subject { Interval.p2(intervals) }
+
+    context 'Test #1' do
+      let(:intervals) { [sched1,
+                         sched2,
+                         sched3,
+                         sched4] }
+      let(:sched1) { Interval.new("8:00", "9:00") }
+      let(:sched2) { Interval.new("10:00", "11:15") }
+      let(:sched3) { Interval.new("11:00", "12:30") }
+      let(:sched4) { Interval.new("13:00", "14:30") }
+
+      it { should eq [
+                       [sched1, sched2, sched4],
+                       [sched1, sched3, sched4],
+                     ] }
+    end
   end
 
   describe "Problem #3" do

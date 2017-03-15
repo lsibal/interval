ff22a7b97a693f9eff28790588e3af40dbc02d02
Glenn J Gonzales <glenn@shieldfoundry.com>
2017-03-15 11:29:07 +0800

    Add speed/performance tests

--- spec/lib/interval_spec.rb
+++ spec/lib/interval_spec.rb
@@ -2,8 +2,34 @@
 
 require 'spec_helper'
 require 'interval'
+require 'benchmark'
 
 describe Interval do
+
+  def gen_intervals(min, max, n)
+    rnd = Random.new
+    xs = []
+    n.times do
+      _start = rnd.rand(min..max)
+      _end = rnd.rand(_start..max)
+      xs << [_start, _end]
+    end
+    xs
+  end
+
+  def gen_string_times(min, max, n)
+    xs = gen_intervals(min, max, n)
+    xs.map do |_start, _end|
+      sh = _start / 60
+      sm = _start % 60
+      s = "#{sh}:#{'%02d' % (sm)}"
+      eh = _end / 60
+      em = _end % 60
+      e = "#{eh}:#{'%02d' % (em)}"
+      Interval.new(s, e)
+    end
+  end
+
   describe "Problem #1" do
     subject { Interval.p1(a, b, x) }
 
@@ -56,6 +82,20 @@ describe Interval do
 
       it { should eq Interval.new(5,26) }
     end
+
+    it "should be fast" do
+      size = 100000
+      xs = gen_intervals(0, 1440, size)
+      is = xs.map { |x| Interval.new(x[0], x[1])}
+      time = Benchmark.measure do
+        n = 5
+        a = is.shift
+        b = is
+        r = Interval.p1(a, b, n)
+      end
+      expect(time.total).to be < 4
+      puts "Interval.p1 finished in #{'%.2f' % time.total}s"
+    end
   end
 
   describe "Problem #2" do
@@ -75,6 +115,17 @@ describe Interval do
                        [sched1, sched2, sched4],
                        [sched1, sched3, sched4],
                      ] }
+    end
+
+    it "should be fast" do
+      size = 100
+      xs = gen_string_times(0, 1440, size)
+      r = []
+      time = Benchmark.measure do
+        r = Interval.p2(xs)
+      end
+      expect(time.total).to be < 4
+      puts "Interval.p2 finished in #{'%.2f' % time.total}s"
     end
   end
 
